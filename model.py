9eef6e5913dad69024cdb259327546bf4506bc39
Walker Lindley <walker@walkerlindley.com>
2012-11-05 22:16:34 -0800

    Stubbing out some more of what the data model should store.

--- model.py
+++ model.py
@@ -3,5 +3,6 @@ from google.appengine.ext import db
 class PlayerModel(db.Model):
 	playerId = db.StringProperty(required=True)
 
-class TurnTrackerModel(db.Model):
-	pass
+class GameModle(db.Model):
+	gameId = db.StringProperty(required=True)
+	serializedGame = db.TextProperty()
