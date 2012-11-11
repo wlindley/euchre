be31a2e80eabb4f0c4deb36de3d4bbe7078bbb7d
Walker Lindley <walker@walkerlindley.com>
2012-11-11 12:00:07 -0800

    Adding model_test module now that I've gotten nosetest with gae running properly.

--- runalltests.sh
+++ runalltests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python -m unittest euchre_test game_test serializer_test
-#nosetests --with-gae game_test.py euchre_test.py serializer_test.py
+#python -m unittest euchre_test game_test serializer_test
+nosetests --with-gae game_test.py euchre_test.py serializer_test.py model_test.py
