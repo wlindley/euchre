98ad3ee51b07e56854bdec62251eac9362318af4
Walker Lindley <walker@walkerlindley.com>
2012-11-13 09:03:46 -0800

    Fixed a typo that prevented executable_test from being run by runalltests.sh.

--- runalltests.sh
+++ runalltests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #python -m unittest euchre_test game_test serializer_test
-nosetests --with-gae game_test.py euchre_test.py serializer_test.py util_test.py executabe_test.py
+nosetests --with-gae game_test.py euchre_test.py serializer_test.py util_test.py executable_test.py
