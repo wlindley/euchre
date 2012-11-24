db17a8d223680f987829e7f35831453dc548f254
Walker Lindley <walker@walkerlindley.com>
2012-11-23 17:25:08 -0800

    Updated runPythonTests.sh to take an optional parameter that specifies a specific test suite to run instead of running all tests.

--- test/runPythonTests.sh
+++ test/runPythonTests.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+filePattern="*.py"
+if [ -n "$1" ]; then
+	filePattern=$1
+fi
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-nosetests --with-gae --gae-application=$DIR/.. $DIR/*.py
+nosetests --with-gae --gae-application=$DIR/.. $DIR/$filePattern
