3d9dd7a55657e727ad7487e35a626918585ac480
wlindley <walker@walkerlindley.com>
2013-10-20 21:25:35 -0700

    Forgot to add new parameters to PlayerNameDirectory in init.js.

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ $(function() {
 		window.jqueryWrapper = AVOCADO.JQueryWrapper.getInstance($);
 		window.ajax = AVOCADO.Ajax.getInstance(window.jqueryWrapper, EUCHRE.ajaxUrl);
 		window.templateRenderer = AVOCADO.TemplateRenderer.getInstance(EUCHRE.templates);
-		window.playerNameDirectory = AVOCADO.PlayerNameDirectory.getInstance(window.ajax);
+		window.playerNameDirectory = AVOCADO.PlayerNameDirectory.getInstance(window.ajax, EUCHRE.locStrings, EUCHRE.playerId);
 		window.viewManager = AVOCADO.ViewManager.getInstance();
 		window.gameListView = AVOCADO.GameListView.getInstance(window.templateRenderer, $('#gameList'), window.jqueryWrapper, window.viewManager, window.ajax, EUCHRE.locStrings, EUCHRE.playerId);
 		window.gamePlayView = AVOCADO.GamePlayView.getInstance(window.ajax, EUCHRE.playerId, window.templateRenderer, $('#gamePlay'), window.viewManager, EUCHRE.locStrings, window.jqueryWrapper);
