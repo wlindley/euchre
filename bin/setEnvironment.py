7c3de6053dc832df525eb2a3cc39d91d2ee2d1ec
wlindley <walker@walkerlindley.com>
2013-12-10 14:10:41 -0800

    Updating setEnvironment.py to use shortnames for environments.

--- bin/setEnvironment.py
+++ bin/setEnvironment.py
@@ -3,12 +3,19 @@
 import os
 import sys
 
-def replaceAppId(yamlFilename, appId):
+def translateShortName(shortname):
+	if "local" == shortname:
+		return "familyeuchre-local"
+	elif "stage" == shortname:
+		return "familyeuchre-staging"
+	return "familyeuchre"
+
+def replaceAppId(yamlFilename, environment):
 	handle = open(yamlFilename, 'r')
 	lines = handle.readlines()
 	for i in range(len(lines)):
 		if lines[i].startswith("application:"):
-			lines[i] = "application: " + appId + "\n"
+			lines[i] = "application: " + translateShortName(environment) + "\n"
 	handle.close()
 	handle = open(yamlFilename, 'w')
 	handle.writelines(lines)
@@ -16,6 +23,6 @@ def replaceAppId(yamlFilename, appId):
 
 if __name__=="__main__":
 	if len(sys.argv) < 3:
-		print "Usage: %s <path/to/app.yaml> <app_id>" % sys.argv[0]
+		print "Usage: %s <path/to/app.yaml> <environment>" % sys.argv[0]
 		sys.exit(1)
 	replaceAppId(sys.argv[1], sys.argv[2])
