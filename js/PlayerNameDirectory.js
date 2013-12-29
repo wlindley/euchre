b2b0100b856cc22998539be83ed7fc51302c4dff
wlindley <walker@walkerlindley.com>
2013-12-29 15:34:40 -0800

    Refactored PlayerNameDirectory for better readability.

--- js/PlayerNameDirectory.js
+++ js/PlayerNameDirectory.js
@@ -8,21 +8,12 @@ AVOCADO.PlayerNameDirectory = function(locStrings, facebook, dataRetriever) {
 	this.getNamePromise = function(pid) {
 		if (!(pid in promises)) {
 			promises[pid] = AVOCADO.PlayerNamePromise.getInstance(pid, locStrings);
+
 			if (facebook.getSignedInPlayerId() == pid) {
 				promises[pid].setName(locStrings["you"]);
 			} else {
-				var displayName = null;
-
-				if (pid) {
-					var robots = dataRetriever.get("robots");
-					for (var i in robots) {
-						if ("" != robots[i].id && 0 == pid.indexOf(robots[i].id)) {
-							var uid = pid.replace(robots[i].id, "").replace(/_/g, "");
-							displayName = robots[i].displayName + " " + uid;
-						}
-					}
-				}
-
+				var displayName = getRobotDisplayName(pid);
+				
 				if (null != displayName) {
 					promises[pid].setName(displayName);
 				} else {
@@ -39,6 +30,19 @@ AVOCADO.PlayerNameDirectory = function(locStrings, facebook, dataRetriever) {
 			promises[pid].setName(response["name"]);
 		}
 	};
+
+	function getRobotDisplayName(pid) {
+		if (pid) {
+			var robots = dataRetriever.get("robots");
+			for (var i in robots) {
+				if ("" != robots[i].id && 0 == pid.indexOf(robots[i].id)) {
+					var uid = pid.replace(robots[i].id, "").replace(/_/g, "");
+					return robots[i].displayName + " " + uid;
+				}
+			}
+		}
+		return null;
+	}
 };
 
 AVOCADO.PlayerNameDirectory.getInstance = function(locStrings, facebook, dataRetriever) {
